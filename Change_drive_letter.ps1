b3b4e85bd0dc797d093bc1d8e0164b81332c818a
Rahul Kaushik <66063169+kaushikrahul08@users.noreply.github.com>
2020-08-07 13:27:36 +0530

    Add files via upload

---
+++ Change_drive_letter.ps1
@@ -0,0 +1,35 @@
+﻿[CmdletBinding()]
+Param(
+[Parameter(Mandatory=$True,
+Position=1)]
+[string]
+[ValidatePattern("^[A-Z]{1}:{1}`$")]$NewDrvLetter
+)
+# Get Available CD/DVD Drive - Drive Type 5
+$DvdDrv = Get-WmiObject -Class Win32_Volume -Filter "DriveType=5"
+ 
+# Check if CD/DVD Drive is Available
+if ($DvdDrv -ne $null)
+{
+ 
+# Get Current Drive Letter for CD/DVD Drive
+$DvdDrvLetter = $DvdDrv | Select-Object -ExpandProperty DriveLetter
+Write-Output "Current CD/DVD Drive Letter is $DvdDrvLetter"
+ 
+# Confirm New Drive Letter is NOT used
+if (-not (Test-Path -Path $NewDrvLetter))
+{
+ 
+# Change CD/DVD Drive Letter
+$DvdDrv | Set-WmiInstance -Arguments @{DriveLetter="$NewDrvLetter"}
+Write-Output "Updated CD/DVD Drive Letter as $NewDrvLetter"
+}
+else
+{
+Write-Output "Error: Drive Letter $NewDrvLetter Already In Use"
+}
+}
+else
+{
+Write-Output "Error: No CD/DVD Drive Available !!"
+}
---
+++ format-disk.ps1
@@ -0,0 +1,16 @@
+﻿function Set-ServerDisk 
+      { Param    
+      ([Parameter(Mandatory=$true)] [string[]] $driveLetter,
+       [Parameter(Mandatory=$True)]     [string[]]$labels)
+        $disks = Get-Disk | Where partitionstyle -eq 'raw' | sort number
+        $count = 0
+        $labels = "data1"
+        $driveLetter= "F"
+       foreach ($disk in $disks)
+       { 
+$disk |Initialize-Disk -PartitionStyle GPT -PassThru |New-Partition -UseMaximumSize -DriveLetter $driveLetter[$count] | Format-Volume -FileSystem NTFS -NewFileSystemLabel $labels[$count] -Confirm:$false -Force 
+         $count++
+           }
+           }
+
+Set-ServerDisk -driveLetter "F" -labels "Data"
